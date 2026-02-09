1bc6b38b333c4e2f5b6e9828c325d9c766f69a88
chaimae12444 <sedraoui.chaimae.solicode@gmail.com>
2026-02-09 09:19:02 +0100

    Add files via upload

---
+++ prototype php/prototype.php
@@ -0,0 +1,69 @@
+<form action="" method="post">
+    <input type="text" name="n1" placeholder="Nombre 1">
+    <input type="text" name="n2" placeholder="Nombre 2">
+
+    <select name="op">
+        <option value="+">Addition</option>
+        <option value="-">Soustraction</option>
+        <option value="*">Multiplication</option>
+        <option value="/">Division</option>
+    </select>
+
+    <button type="submit" name="calc">Calculer</button>
+
+    <br><br>
+    <input type="text" readonly value="<?php if(isset($res)) echo $res; ?>">
+</form>
+<?php
+
+function validerNombre($x){
+    return is_numeric($x);
+}
+
+function calculer($a, $b, $op, &$msgErr){
+    if(!validerNombre($a) || !validerNombre($b)){
+        $msgErr = "Erreur: veuillez saisir des nombres valides.";
+        return null;
+    }
+
+    switch($op){
+        case '+':
+            return $a + $b;
+        case '-':
+            return $a - $b;
+        case '*':
+            return $a * $b;
+        case '/':
+            if($b == 0){
+                $msgErr = "Erreur: division par zéro impossible !";
+                return null;
+            }
+            return $a / $b;
+        default:
+            $msgErr = "Opération inconnue.";
+            return null;
+    }
+}
+
+if(isset($_POST['calc'])){
+    $n1 = $_POST['n1'];
+    $n2 = $_POST['n2'];
+    $op = $_POST['op'];
+
+    $msgErr = "";
+    $resultat = calculer($n1, $n2, $op, $msgErr);
+
+    if($msgErr != ""){
+        $res = $msgErr;
+    } else {
+        $res = "Résultat de la $op = $resultat";
+    }
+}
+?>
+$libelle = [
+    "+" => "Addition",
+    "-" => "Soustraction",
+    "*" => "Multiplication",
+    "/" => "Division"
+];
+$res = $libelle[$op] . " = $resultat";
